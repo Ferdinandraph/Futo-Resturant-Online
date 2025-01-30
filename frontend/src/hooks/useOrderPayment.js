ca1cf0b19f953b30d17387992950c6a8cc8a2a5d
Ferdinandraph <ferdinandraphael20@gmail.com>
2025-01-30 10:34:17 -0800

    new files added

--- frontend/src/hooks/useOrderPayment.js
+++ frontend/src/hooks/useOrderPayment.js
@@ -36,7 +36,7 @@ const useOrderPayment = () => {
 
       // Initialize Payment
       const { data } = await axios.post(
-        "http://0.0.0.0:1000/api/paystack/initialize",
+        "http://localhost:3300/api/paystack/initialize",
         paymentData,
         {
           headers: {
