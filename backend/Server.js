e71f795b9f2bad38de883c8b5e96dbf112aa5a55
Ferdinandraph <ferdinandraphael20@gmail.com>
2025-01-30 09:53:23 -0800

    new files added

--- backend/Server.js
+++ backend/Server.js
@@ -35,5 +35,5 @@ app.use((err, req, res, next) => {
 
 // Start the server
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
